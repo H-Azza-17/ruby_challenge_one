ef62adae6464b1525bfcecc5a45489739413d088
Aaron <harrisaaron321@gmail.com>
2022-12-05 11:32:25 +0000

    adds implementation

--- lib/password_validator.rb
+++ lib/password_validator.rb
@@ -3,9 +3,4 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
-    if password.length >= 8 and password[/[!@$%&]/] # this check if the length is more than or equal to 8 and if there are any of the following characters: !, @, $, %, &
-        return true # returns true if the password is valid.
-    else
-        return false # returns false if the password is not valid.
-    end
 end
